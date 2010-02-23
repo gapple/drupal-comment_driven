a39f1a6a17afeac91728c44f628ca4945a743b7c
Michael OBrien <michael.obrien@praece.com>
2010-02-23 02:46:20 +0000

    obrienmd: Commit unstable2

---
+++ comment_driven.js
@@ -0,0 +1,25 @@
+// $Id$
+
+Drupal.comment_driven = Drupal.comment_driven || {};
+
+Drupal.comment_driven.node_wrap = function() {
+  // the comment_form is within id=comment_driven
+  // everything else belongs to node_form
+  
+  // #weight was already provided to make
+  // id=comment_driven ligther and id=driven_node_container heavier
+  // but we wan't to reorder them both within the form
+  // therefore, we'll bring out the the two major pieces (comment_form and empty fieldset)
+  // to be able to select everything else to go into id=driven_node_container
+  $('#comment_driven').prependTo('#comment-form');
+  $('#driven_node_container').prependTo('#comment-form');
+  
+  // bring every node_form element into the fieldset
+  // don't rely on class=node-form being set by a theme function
+  $('#comment-form > div:not(#comment_driven)').prependTo('#driven_node_container .fieldset-wrapper');
+  // make the fieldset visible
+  $('#driven_node_container').css('display', 'block');
+};
+
+// just once (not a behavior)
+$(document).ready(Drupal.comment_driven.node_wrap);
